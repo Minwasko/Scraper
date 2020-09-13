71529af9e1a24e39acf9f2d03f97adab9ed6c01f
Minwas <you@example.com>
2020-09-13 15:29:04 +0300

    json working

--- Scrap/src/Product.java
+++ Scrap/src/Product.java
@@ -1,14 +1,20 @@
 public class Product {
 
-    private String title;
-    private String information;
-    private String picture;
-
-    public Product(String title, String information, String picture) {
-        this.title = title;
-        this.information = information;
-        this.picture = picture;
-    }
+    private String title; //
+    private String lineTitle; //
+    private String productPurpose; //
+    private String productLink; //
+    private String productPicture; //
+    private String productInformation; //
+    private String productBrand; //
+    private String productVolume; //
+    private String productBarcode; //
+    private String productComposition; //
+    private String productNavigationOne;
+    private String productNavigationTwo;
+    private String productNavigationThree;
+    private String productNavigationFour;
+    private String productNavigationFive;
 
     public String getTitle() {
         return title;
@@ -18,28 +24,136 @@ public class Product {
         this.title = title;
     }
 
-    public String getInformation() {
-        return information;
+    public String getLineTitle() {
+        return lineTitle;
     }
 
-    public void setInformation(String information) {
-        this.information = information;
+    public void setLineTitle(String lineTitle) {
+        this.lineTitle = lineTitle;
     }
 
-    public String getPicture() {
-        return picture;
+    public String getProductPurpose() {
+        return productPurpose;
     }
 
-    public void setPicture(String picture) {
-        this.picture = picture;
+    public void setProductPurpose(String productPurpose) {
+        this.productPurpose = productPurpose;
+    }
+
+    public String getProductLink() {
+        return productLink;
+    }
+
+    public void setProductLink(String productLink) {
+        this.productLink = productLink;
+    }
+
+    public String getProductPicture() {
+        return productPicture;
+    }
+
+    public void setProductPicture(String productPicture) {
+        this.productPicture = productPicture;
+    }
+
+    public String getProductInformation() {
+        return productInformation;
+    }
+
+    public void setProductInformation(String productInformation) {
+        this.productInformation = productInformation;
+    }
+
+    public String getProductBrand() {
+        return productBrand;
+    }
+
+    public void setProductBrand(String productBrand) {
+        this.productBrand = productBrand;
+    }
+
+    public String getProductVolume() {
+        return productVolume;
+    }
+
+    public void setProductVolume(String productVolume) {
+        this.productVolume = productVolume;
+    }
+
+    public String getProductBarcode() {
+        return productBarcode;
+    }
+
+    public void setProductBarcode(String productBarcode) {
+        this.productBarcode = productBarcode;
+    }
+
+    public String getProductComposition() {
+        return productComposition;
+    }
+
+    public void setProductComposition(String productComposition) {
+        this.productComposition = productComposition;
+    }
+
+    public String getProductNavigationOne() {
+        return productNavigationOne;
+    }
+
+    public void setProductNavigationOne(String productNavigationOne) {
+        this.productNavigationOne = productNavigationOne;
+    }
+
+    public String getProductNavigationTwo() {
+        return productNavigationTwo;
+    }
+
+    public void setProductNavigationTwo(String productNavigationTwo) {
+        this.productNavigationTwo = productNavigationTwo;
+    }
+
+    public String getProductNavigationThree() {
+        return productNavigationThree;
+    }
+
+    public void setProductNavigationThree(String productNavigationThree) {
+        this.productNavigationThree = productNavigationThree;
+    }
+
+    public String getProductNavigationFour() {
+        return productNavigationFour;
+    }
+
+    public void setProductNavigationFour(String productNavigationFour) {
+        this.productNavigationFour = productNavigationFour;
+    }
+
+    public String getProductNavigationFive() {
+        return productNavigationFive;
+    }
+
+    public void setProductNavigationFive(String productNavigationFive) {
+        this.productNavigationFive = productNavigationFive;
     }
 
     @Override
     public String toString() {
         return "Product{" +
                 "title='" + title + '\'' +
-                ", information='" + information + '\'' +
-                ", picture='" + picture + '\'' +
+                ", lineTitle='" + lineTitle + '\'' +
+                ", productPurpose='" + productPurpose + '\'' +
+                ", productLink='" + productLink + '\'' +
+                ", productPicture='" + productPicture + '\'' +
+                ", productInformation='" + productInformation + '\'' +
+                ", productBrand='" + productBrand + '\'' +
+                ", productVolume='" + productVolume + '\'' +
+                ", productBarcode='" + productBarcode + '\'' +
+                ", productComposition='" + productComposition + '\'' +
+                ", productNavigationOne='" + productNavigationOne + '\'' +
+                ", productNavigationTwo='" + productNavigationTwo + '\'' +
+                ", productNavigationThree='" + productNavigationThree + '\'' +
+                ", productNavigationFour='" + productNavigationFour + '\'' +
+                ", productNavigationFive='" + productNavigationFive + '\'' +
                 '}';
     }
 }
--- Scrap/src/Scraper.java
+++ Scrap/src/Scraper.java
@@ -1,6 +1,10 @@
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -18,34 +22,65 @@ public class Scraper {
     public void getAllLines() throws IOException {
         Elements lines = Jsoup.connect(URL_START + "/en/brendy/").get().select("div.brands-list");
         for (Element line : lines.select("div.item")) {
-            System.out.println(line.select("a.item_w").attr("href"));
             getProductsForLine(URL_START + line.select("a.item_w").attr("href"),
                     line.select("a.item_w").attr("title"));
+            break;
         }
+        Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+        FileWriter writer = new FileWriter("C:\\Users\\Minwa\\IdeaProjects\\Scraper\\"
+                + "Scrap\\src\\jsonInfo.json");
+        writer.write(gson.toJson(this.lines));
+        writer.close();
+        System.out.println(this.lines);
     }
 
     public void getProductsForLine(String lineURL, String lineTitle) throws IOException {
         Elements products = Jsoup.connect(lineURL).get().select("div.sl-catalog-wrap");
         Line line = new Line(lineTitle);
-        System.out.println(line.getTitle());
         lines.add(line);
         for (Element product : products.select("div.slide")) {
-            getProductInfo(URL_START + product.select("a").attr("href"));
-            break;
+            Product wantedProduct = new Product();
+            Elements productSpans = product.select("span.text");
+            if (productSpans.size() > 1) {
+            wantedProduct.setProductPurpose(product.select("div.descr-param").select("span.text").text()); // ADDED PRODUCT PURPOSE
+            } else {
+                wantedProduct.setProductPurpose("No purpose given"); // ADDED PRODUCT PURPOSE IF NONE WAS GIVEN
+            }
+            wantedProduct.setTitle(product.select("div.ttl").text()); // ADDED PRODUCT TITLE
+            wantedProduct.setLineTitle(lineTitle); // ADDED PRODUCT LINE TITLE
+            getProductInfo(URL_START + product.select("a").attr("href"), wantedProduct);
+            line.addProduct(wantedProduct);
         }
+        lines.add(line);
 
     }
 
-    public Product getProductInfo(String productURL) throws IOException {
+    public Product getProductInfo(String productURL, Product product) throws IOException {
+        System.out.println(productURL);
+        product.setProductLink(productURL); // ADDED PRODUCT LINK
         Elements productInfo = Jsoup.connect(productURL).get().select("div.product");
-        String productTitle = productInfo.select("div.product-head").select("h1.title").get(0).text();
-        System.out.println(productTitle);
-        String productInformation = productInfo.select("div.product-info").select("div.tab-content").text();
-        System.out.println(productInformation.replace("Brend", " :Information | Characteristics: "));
-        String productPicture = productInfo.select("div.product-pic").select("div.product-pic-wrap").select("a[href]")
-                .attr("href");
-        System.out.println(URL_START + productPicture);
-        return null;
+        product.setProductPicture(productInfo.select("div.product-pic").select("div.product-pic-wrap").select("a[href]")
+                .attr("href")); // ADDED PRODUCT PICTURE
+        product.setProductInformation(productInfo.select("div.product-tab").select("div#product1").toString().replace("<p>", "")
+                .replace("</p>", "").replace("<br>", "").replace("<div>", "")
+                .replace("</div>", "").replace("<div role=\"tabpanel\" class=\"tab-pane fade in active\" id=\"product1\">", "")
+                .replace("<b>", "").replace("</b>", "")); // ADDED PRODUCT INFORMATION
+        Elements productCharacteristics = productInfo.select("div.product-tab").select("div#product2").select("p");
+        product.setProductBrand(productCharacteristics.get(0).text()); // ADDED PRODUCT BRAND
+        product.setProductVolume(productCharacteristics.get(1).text()); // ADDED PRODUCT VOLUME
+        product.setProductBarcode(productCharacteristics.get(2).text()); // ADDED PRODUCT BARCODE
+        product.setProductComposition(productCharacteristics.get(3).text()); // ADDED PRODUCT COMPOSITION
+        // Getting the navigation tab
+        Elements productNavigationTabs = productInfo.select("div.breadcrumbs").select("li.item");
+        try {
+            product.setProductNavigationOne(productNavigationTabs.get(0).text());
+            product.setProductNavigationTwo(productNavigationTabs.get(1).text());
+            product.setProductNavigationThree(productNavigationTabs.get(2).text());
+            product.setProductNavigationFour(productNavigationTabs.get(3).text());
+            product.setProductNavigationFive(productNavigationTabs.get(4).text());
+        } catch (Exception ignored) {
+        }
+        return product;
     }
 
     public static void main(String[] args) throws IOException {
